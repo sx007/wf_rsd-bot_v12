5be7ec5aaeea163e7914c56d7edc30813ad7c82c
Ivan <42902953+sx007@users.noreply.github.com>
2020-10-30 20:57:07 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,43 @@
+const Discord = require('discord.js'); 
+const bot = new Discord.Client();
+//подключаем файл конфигурации
+let config = require('./botconfig.json'); 
+//"достаём" токен и префикс
+let token = config.token; 
+let prefix = config.prefix;
+//создаём ссылку-приглашение для бота
 
+bot.on('ready', () => { 
+    // Set the client user's presence
+    bot.user.setPresence({ activity: { name: 'Warface RU' }, status: 'online' })
+    //.then(console.log)
+    //.catch(console.error);
+
+
+    console.log(`Запустился бот ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => { 
+        console.log(link);
+    });
+});
+
+bot.on("message", function(message) {
+if (message.author.bot) return;
+if (!message.content.startsWith(prefix)) return;
+
+const commandBody = message.content.slice(prefix.length);
+const args = commandBody.split(' ');
+const command = args.shift().toLowerCase();
+
+if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! Время генерации сообщения ${timeTaken}ms.`);
+}
+
+else if (command === "sum") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.length;
+    message.reply(`Количество аргуметов: ${sum}!`);
+}
+});
+
+bot.login(token);
